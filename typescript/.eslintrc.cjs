3d2278345cd66d574fbab75aac13c686fae4c4b8
Lucas Matuszewski <lucas.matuszewski@gmail.com>
2025-06-10 02:29:08 +0100

    Node TS - fix eslint issue (use cjs and __dirname) + remove unused pnpm workspace file

---
+++ typescript/.eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'all'],
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['.eslintrc.{js,cjs,json}'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ],
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', '*.js'],
+};
